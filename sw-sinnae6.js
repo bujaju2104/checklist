4819e51c1b53edc2a6334d787845f542256f02d4
bujaju2104 <son21046943@gmail.com>
2026-08-18 08:26:10 +0900

    Add files via upload

--- sw-sinnae6.js
+++ sw-sinnae6.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='sinnae6-pwa-final-v1';
-const ASSETS=['./sinnae6-checklist.html?pwa-final','./manifest-sinnae6.json','./icon-gwangmyeong-192.png','./icon-gwangmyeong-512.png'];
+const CACHE_NAME='sinnae6-app-v2';
+const ASSETS=['./sinnae6-checklist.html?sinnae6-app-v2','./manifest-sinnae6.json','./icon-gwangmyeong-192.png','./icon-gwangmyeong-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS).catch(()=>{})).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('sinnae6-')&&k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
